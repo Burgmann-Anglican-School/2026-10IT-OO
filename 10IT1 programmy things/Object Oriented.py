d730cbbf0e3aa14917d0494ac065e91ccf001749
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2026-09-09 11:46:15 +1000

    did more work

--- 10IT1 programmy things/Object Oriented.py
+++ 10IT1 programmy things/Object Oriented.py
@@ -19,26 +19,98 @@ class Dog:
     # the double underscore is a special python identifier called a 
         # dunder.
     #Most methods must have self as an argument
-    def __init__(self, legs, breed, colour, height, age, name):
+    def __init__(self, legs, colour, height, age, name):
         #We take the arguments as input to the function and connect them
         # to the class itself
         self.legs = legs
-        self.breed = breed
         self.colour = colour
         self.height = height
         self.age = age
         self.name = name
 
+    #This funtion takes another dog object (other) and lets the first dog talk to the second
+    # we use it like this: dog1.converse(dog2)
     def converse(self, other):
         print(f"{self.name} says 'Hi' to {other.name}")
 
-    def __str__(self):
-        return f"{self.name} is a {self.breed} that is {self.age} years old"
+    #This function lets a dog say the words you write into the function as an argument
+    # we use it like dog1.speak("put cool words here")
+    def speak(self, words):
+        return f'{self.name} says {words}'
 
-my_dog = Dog(4, "Golden Retriever", "blonde", '7ft', 13, "Clifford the dying dog")
-my_dog2 = Dog(4, "Caboodle", "orange", '2ft', 1, "Gerald the flying dog")
-my_dog3 = Dog(4, "Toy poodle", "yellow", '1ft', 3, "Garry the frying dog")
-my_dog4 = Dog(4, "Daschund", "green", '3ft', 5, "Emanuel the lying dog")
+    #This is a dunder str, it overrides the print function    
+    #When print is called on instances of this class, it runs
+    # this function
+    def __str__(self):
+        return f"{self.name} is a dog that is {self.age} years old"
+
+#These are child classes. Dog is the parent class
+#They will do everything dog does, but they override the speak method
+class GoldenRetriver(Dog):
+    #GoldenRetrievers cannot speak like a dog, they just "bark"
+    def speak(self):
+        return "bark"
+
+#This overrides nothing and is identical to Dog
+class Caboodle(Dog):
+    pass
+
+class ToyPoodle(Dog):
+    #This version of speak defaults to yap if no words are provided
+    def speak(self, words='yap'):
+        #super referes to the parent class
+        return super().speak(words)
+        
+class Dachshund(Dog):
+    def speak(self):
+        return "sausage"
+        
+my_dog = GoldenRetriver(4, "blonde", '7ft', 13, "Clifford the dying dog")
+my_dog2 = Caboodle(4, "orange", '2ft', 1, "Gerald the flying dog")
+my_dog3 = ToyPoodle(4, "yellow", '1ft', 3, "Garry the frying dog")
+my_dog4 = Dachshund(4, "green", '3ft', 5, "Emanuel the lying dog")
 
 my_dog.converse(my_dog2)
 print(my_dog)
+
+print(my_dog.speak())
+print(my_dog2.speak('words'))
+print(my_dog3.speak())
+print(my_dog4.speak())
+
+#Car question/answer below
+class Car:
+
+    def __init__(self, colour, kilometerage, speed):
+        self.colour = colour
+        self.k = kilometerage
+        self.speed = speed
+
+    def __str__(self):
+        return f"The {self.colour} car has {self.k} kilometres."
+
+    def vroom(self):
+        return self.speed
+
+    def difference(self, other):
+        return abs(self.k - other.k)
+
+    def compare_speed(self, other):
+        return abs(self.speed - other.speed)
+
+class Toyota(Car):
+    pass
+
+class BMW(Car):
+    def compare_speed(self, other):
+        return "always faster"
+
+blue_car = Toyota("blue", 20000, 100)
+red_car = BMW("red", 30000, 120)
+
+print(blue_car)
+print(red_car)
+print(blue_car.difference(red_car))
+print(red_car.speed)
+print(blue_car.compare_speed(red_car))
+print(red_car.compare_speed(blue_car))
--- 10IT2 priogam stuf/stuff.py
+++ 10IT2 priogam stuf/stuff.py
@@ -7,7 +7,35 @@ dog_other_appendages = 2
 #Dog implementation with dictionaries
 bazza = {'legs': 4, 'colour': 'brown', 'breed':'husky'}
 
+#creating a class
 class Dog:
 
-    def __init__(self):
-        pass
+    #this is the dunder init, iot initialises the class
+    #the first argument will always be self,  this lets the class
+    # refer to itself
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    #This is a new method that lets the dog say something
+    def speak(self, words):
+        return f"{self.name} says {words}"
+
+    #This is a new method that takes 2 dogs, one as self, the other as other_dog
+    def converse(self, other_dog):
+        return f"{self.name} says hi to {other_dog.name}"
+
+    #This is the dunder str, it overrides the print function
+    def __str__(self):
+        return f"{self.name} is a {self.age} year old {self.breed}"
+
+my_dog = Dog('Markus', 20, 'labradoodle')
+dog2 = Dog('Markuce', 47, 'pug')
+
+print(my_dog.name)
+print(my_dog.age)
+print(my_dog.breed)
+print(my_dog)
+print(my_dog.speak('Hello'))
+print(my_dog.converse(dog2))
